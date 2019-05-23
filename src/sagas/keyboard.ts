a6b6365936fad0162701a1fbad550c7f3694042c
aoba <ipbdipbd00@gmail.com>
2019-05-24 01:41:32 +0900

    Fix saga

--- src/sagas/keyboard.ts
+++ src/sagas/keyboard.ts
@@ -2,81 +2,51 @@ import {
   fork,
   put,
   all,
-  call,
   take,
   select,
   cancel,
-  takeEvery,
+  delay,
 } from 'redux-saga/effects';
 import * as Actions from '../actions/keyboardConstants';
 import { update } from '../actions/plaryer';
-// import {  } from '../actions/keyboard';
-
-// function* runGetMembers(action: ReturnType<typeof getMembers.start>) {
-//   const { organizationName } = action.payload.params;
-//   const api = getMembersFactory();
-
-//   try {
-//     const users = yield call(api, organizationName);
-//     yield put(getMembers.succeed({ organizationName }, { users }));
-//   } catch (error) {
-//     yield put(getMembers.fail({ organizationName }, error));
-//   }
-// }
 
 const velocity = 5;
 
-// function* increment(keyboard: any, player: any) {
-// setInterval(() => {
-//   let { x, y } = player;
-//   if (keyboard.up) y -= velocity;
-//   if (keyboard.down) y += velocity;
-//   if (keyboard.left) x -= velocity;
-//   if (keyboard.right) x += velocity;
-// }, 400);
-// yield put(update({ x, y }));
-// const runner = yield call(
-//   setInterval,
-//   () => {
-//     if (keyboard.up) y -= velocity;
-//     if (keyboard.down) y += velocity;
-//     if (keyboard.left) x -= velocity;
-//     if (keyboard.right) x += velocity;
-//     yield put(update({ x, y }));
-//   },
-//   400,
-// );
-// }
+function* increment() {
+  while (true) {
+    const { keyboard, player } = yield select(state => state);
+    let { x, y }: { x: number; y: number } = player;
 
-function* startMove() {
-  const { keyboard, player } = yield select(state => state);
-  let { x, y } = player;
-  if (keyboard.up) y -= velocity;
-  if (keyboard.down) y += velocity;
-  if (keyboard.left) x -= velocity;
-  if (keyboard.right) x += velocity;
-  yield put(update({ x, y }));
-  // const task = yield fork(increment, keyboard, player);
-  // yield take([
-  //   Actions.DOWN_STOP,
-  //   Actions.LEFT_STOP,
-  //   Actions.RIGHT_STOP,
-  //   Actions.UP_STOP,
-  // ]);
-  // yield cancel(task);
+    if (keyboard.up) y -= velocity;
+    if (keyboard.down) y += velocity;
+    if (keyboard.left) x -= velocity;
+    if (keyboard.right) x += velocity;
+
+    yield put(update({ x, y }));
+    yield delay(1000 / 60);
+  }
 }
 
-function* keyboardWatcher() {
-  const action = yield takeEvery(
-    [
-      Actions.DOWN_START,
-      Actions.LEFT_START,
-      Actions.RIGHT_START,
-      Actions.UP_START,
-    ],
-    startMove,
-  );
-  // yield fork(startMove);
+function* startMove() {
+  const task = yield fork(increment);
+  yield take([
+    Actions.DOWN_STOP,
+    Actions.LEFT_STOP,
+    Actions.RIGHT_STOP,
+    Actions.UP_STOP,
+  ]);
+  yield cancel(task);
+  yield fork(keyboardWatcher);
+}
+
+function* keyboardWatcher(): any {
+  yield take([
+    Actions.DOWN_START,
+    Actions.LEFT_START,
+    Actions.RIGHT_START,
+    Actions.UP_START,
+  ]);
+  yield fork(startMove);
 }
 
 export default function* rootSaga() {
